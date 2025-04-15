9ba5ec7bd518ab82c1df69f14abd06245d55b4b5
Almiraadnan <almiraadnan34@gmail.com>
2025-04-15 18:09:38 +0500

    some-code:

---
+++ calculator.js
@@ -0,0 +1,37 @@
+
+// Target Any Element in the Document
+
+// let container = document.getElementById("container")
+// let main = document.getElementsByClassName("main")
+// let span = document.getElementsByTagName("span")
+// var p = document.childNodes[1].childNodes[1].childNodes[1].firstChild.innerHTML = "Bye";
+// var contents = p.innerHTML;
+
+
+// console.log(p);
+
+
+
+// let para = document.getElementById('para')
+// console.log(para.childNodes);
+// function printing() {
+//     window.print()
+// }
+
+
+// console.log(para.parentNode.style.backgroundColor = "red")
+
+
+
+// let inpAttr = input.hasAttribute('src')
+// console.log(inpAttr);
+// console.log(input.attributes);
+
+
+// input.setAttribute('class' , "input-container")
+
+
+function changeInp(num) {
+    let input = document.getElementByIad("input")
+    input.value += num
+}
